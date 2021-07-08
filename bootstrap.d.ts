bb6cce3d51b77ec2b24c20616011ac2f67eae4fd
Riccardo Forina <rforina@redhat.com>
2021-07-08 16:36:24 +0200

    New release

--- bootstrap.d.ts
+++ bootstrap.d.ts
@@ -1,1 +1,9 @@
-import '@patternfly/react-core/dist/styles/base.css';
+import '@patternfly/patternfly/patternfly.css';
+import '@patternfly/patternfly/utilities/Accessibility/accessibility.css';
+import '@patternfly/patternfly/utilities/Sizing/sizing.css';
+import '@patternfly/patternfly/utilities/Spacing/spacing.css';
+import '@patternfly/patternfly/utilities/Display/display.css';
+import '@patternfly/patternfly/utilities/Flex/flex.css';
+import '@patternfly/patternfly/utilities/BackgroundColor/BackgroundColor.css';
+import '@cos-ui/app/dist/cos-ui-app.css';
+import '@cos-ui/creation-wizard/dist/cos-ui-creation-wizard.css';
--- federated.d.ts
+++ federated.d.ts
@@ -1,2 +1,4 @@
+import '@cos-ui/app/dist/cos-ui-app.css';
+import '@cos-ui/creation-wizard/dist/cos-ui-creation-wizard.css';
 import { FederatedApp } from '@cos-ui/app';
 export default FederatedApp;
