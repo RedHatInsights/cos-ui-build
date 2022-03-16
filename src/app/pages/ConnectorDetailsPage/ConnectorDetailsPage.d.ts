02c9d64ce5b4fe10e914b3243e08d3896733aa65
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-03-16 14:36:52 +0000

    Update to bec3147

--- src/app/pages/ConnectorDetailsPage/ConnectorDetailsPage.d.ts
+++ src/app/pages/ConnectorDetailsPage/ConnectorDetailsPage.d.ts
@@ -3,8 +3,12 @@ import { Connector } from '@rhoas/connector-management-sdk';
 export interface ParamTypes {
     id: string;
 }
-export declare const ConnectorDetailsPage: FC;
+declare type ConnectorDetailsPageProps = {
+    onSave: () => void;
+};
+export declare const ConnectorDetailsPage: FC<ConnectorDetailsPageProps>;
 export declare type ConnectorDetailsHeaderProps = {
     connectorData: Connector;
 };
 export declare const ConnectorDetailsHeader: FC<ConnectorDetailsHeaderProps>;
+export {};
---
+++ src/utils/shared.d.ts
@@ -0,0 +1,1 @@
+export declare const mapToObject: (inputMap: Map<string, unknown>) => object;
