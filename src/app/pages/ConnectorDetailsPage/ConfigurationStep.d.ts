49023b4432d438910e87a92c575c39907d7e5349
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-04-06 11:34:44 +0000

    Update to 1066c1c

--- src/app/pages/ConnectorDetailsPage/ConfigurationStep.d.ts
+++ src/app/pages/ConnectorDetailsPage/ConfigurationStep.d.ts
@@ -1,7 +1,4 @@
 import { FC } from 'react';
-export declare type ConfigurationType = {
-    [key: string]: any;
-};
 export declare type ConfigurationStepProps = {
     editMode: boolean;
     schema: Record<string, any>;
