81223fa04a7c12bea57cfa070153d4b42d603e4c
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-04-13 11:34:14 +0000

    Update to 872975f

--- src/app/components/ConnectorStatus/ConnectorStatus.d.ts
+++ src/app/components/ConnectorStatus/ConnectorStatus.d.ts
@@ -1,21 +1,9 @@
 import { FunctionComponent } from 'react';
-import './ConnectorStatus.css';
 declare type ConnectorStatusProps = {
     name: string;
     status: string;
 };
 export declare const ConnectorStatus: FunctionComponent<ConnectorStatusProps>;
 export declare const ConnectorStatusIcon: FunctionComponent<ConnectorStatusProps>;
-export declare enum ConnectorStatuses {
-    Ready = "ready",
-    Failed = "failed",
-    Assigning = "assigning",
-    Assigned = "assigned",
-    Updating = "updating",
-    Provisioning = "provisioning",
-    Deleting = "deleting",
-    Deleted = "deleted",
-    Disconnected = "disconnected"
-}
 export declare function useConnectorStatusLabel(status: string): string;
 export {};
