8ead9cf4f47351e4e5c3716419aa0b56c6152e94
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-05-23 14:36:51 +0000

    Update to 6df41d9

--- src/app/components/ConnectorStatus/ConnectorStatus.d.ts
+++ src/app/components/ConnectorStatus/ConnectorStatus.d.ts
@@ -1,14 +1,20 @@
-import { FC } from 'react';
+import { FC, ReactNode } from 'react';
 import './ConnectorStatus.css';
 declare type ConnectorStatusProps = {
     desiredState: string;
     name: string;
     state: string;
+    clickable?: boolean;
+    popoverBody?: ReactNode;
+    popoverHeader?: ReactNode;
 };
 export declare const ConnectorStatus: FC<ConnectorStatusProps>;
 declare type ConnectorStatusLabelProps = {
     desiredState: string;
     state: string;
+    clickable: boolean | undefined;
+    popoverBody: ReactNode;
+    popoverHeader: ReactNode;
 };
 export declare const ConnectorStatusLabel: FC<ConnectorStatusLabelProps>;
 declare type ConnectorStatusIconProps = {
