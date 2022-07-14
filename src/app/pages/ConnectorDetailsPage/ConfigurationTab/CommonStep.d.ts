e707947b98f68ba27eb224e5a1a274eb334a7145
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-07-14 15:37:26 +0000

    Update to 453eeee

--- src/app/pages/ConnectorDetailsPage/ConfigurationTab/CommonStep.d.ts
+++ src/app/pages/ConnectorDetailsPage/ConfigurationTab/CommonStep.d.ts
@@ -1,8 +1,13 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 export declare type CommonStepProp = {
     editMode: boolean;
     configuration: any;
     changeIsValid: (isValid: boolean) => void;
     onUpdateConfiguration: (type: string, update: any) => void;
 };
+export declare type SecretPlaceholderProp = {
+    SAPlaceholder: string;
+    onSAPlaceholderChange: (value: string, event: React.FormEvent<HTMLInputElement>) => void;
+};
+export declare const SecretPlaceholder: FC<SecretPlaceholderProp>;
 export declare const CommonStep: FC<CommonStepProp>;
