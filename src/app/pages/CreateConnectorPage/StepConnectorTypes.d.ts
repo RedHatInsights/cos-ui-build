f26dc8bb662f35e4947e4c23be14e523023237a9
Red Hat Application Services CI <app-services-ci@users.noreply.github.com>
2022-07-20 12:39:30 +0000

    Update to 1777fea

--- src/app/pages/CreateConnectorPage/StepConnectorTypes.d.ts
+++ src/app/pages/CreateConnectorPage/StepConnectorTypes.d.ts
@@ -1,3 +1,4 @@
 /// <reference types="react" />
+import './StepConnectorTypes.css';
 export declare function SelectConnectorType(): JSX.Element | null;
 export declare function ConnectorTypesGallery(): JSX.Element;
